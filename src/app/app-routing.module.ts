a1d79e0f249e47083a4a3efbfdd8e9aab515ae82
tharun <tharunbattina@gmail.com>
2026-03-10 19:20:05 +0530

    added new components of navigating & commoncomponents

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './navigatingcomponents/home/home.component';
+import { AboutComponent } from './navigatingcomponents/about/about.component';
+import { ProjectsComponent } from './navigatingcomponents/projects/projects.component';
+import { AchievementsComponent } from './navigatingcomponents/achievements/achievements.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'about',
+    component:AboutComponent
+  },
+  {
+    path:'projects',
+    component:ProjectsComponent
+  },
+  {
+    path:'achievements',
+    component:AchievementsComponent
+  },
+  {
+    path:'**',
+    component:HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './navigatingcomponents/home/home.component';
+import { ProjectsComponent } from './navigatingcomponents/projects/projects.component';
+import { AboutComponent } from './navigatingcomponents/about/about.component';
+import { AchievementsComponent } from './navigatingcomponents/achievements/achievements.component';
+import { NavbarComponent } from './commoncomponents/navbar/navbar.component';
+import { FooterComponent } from './commoncomponents/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    AboutComponent,
+    AchievementsComponent,
+    NavbarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
